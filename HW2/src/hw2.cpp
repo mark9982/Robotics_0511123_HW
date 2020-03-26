86af300d8b1deee5f68a390738e937903b1d048c
mark9982 <mark19980617@gmail.com>
2020-03-26 11:22:32 +0800

    Add files via upload
    Week_2

---
+++ HW2/src/hw2.cpp
@@ -0,0 +1,82 @@
+// include ros library
+#include "ros/ros.h"
+
+// include msg library
+#include <turtlesim/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Point.h>
+
+// include math 
+#include <math.h>
+
+using namespace std;
+
+turtlesim::Pose pose;
+geometry_msgs::Twist vel_msg;
+geometry_msgs::Point goal_point;
+//Define a data structure to 3D
+struct XYZ{
+  float x;
+  float y;
+  float z;
+};
+//Declare a variable.Its name is pos_err with XYZ data type
+struct XYZ pos_err;
+
+// declare call back function(call back the pose of robot)
+void pos_cb(const turtlesim::Pose::ConstPtr& msg)
+{
+  pose = *msg;
+} 
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "tutorial_1");
+  ros::NodeHandle n;
+
+  // declare publisher & subscriber
+  ros::Subscriber pos_sub = n.subscribe<turtlesim::Pose>("turtle1/pose", 10, pos_cb);
+  ros::Publisher turtlesim_pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 100);
+  //input your desired position
+  ROS_INFO("Please input (x,y). x>0,y>0");
+  cout<<"desired_X:";
+  cin>>goal_point.x;
+  cout<<"desired_Y:";
+  cin>>goal_point.y;
+  // setting frequency as 10 Hz
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+  while (ros::ok()){
+
+    printf("\ncount : %d\n",count);
+    printf("goal x : %f \t y : %f\n",goal_point.x,goal_point.y);
+    printf("pose x : %f \t y : %f\n",pose.x,pose.y);
+
+    // Calculate position error(feedback term)
+    pos_err.x = goal_point.x - pose.x;
+    pos_err.y = goal_point.y - pose.y;
+    
+    double pos;
+    pos=sqrt(pow(pos_err.x,2)+pow(pos_err.y,2));      
+    vel_msg.linear.x = 0.85*pos;
+    vel_msg.angular.z = 3.55*(atan2(pos_err.y,pos_err.x)-pose.theta);
+    
+    printf("%f",vel_msg.linear.x);
+    printf("%f",vel_msg.angular.z);
+
+    if(pos<=0.06){
+	break;
+    }
+
+    turtlesim_pub.publish(vel_msg);
+
+    count ++;
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
+
+
+
